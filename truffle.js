61292220446f19f10e2ecfd3dc440255c05df573
Alexey Shagaleev <alexey.shagaleev@yandex.ru>
2018-05-15 15:17:31 +0500

    min change truffle config

--- truffle.js
+++ truffle.js
@@ -2,13 +2,21 @@ require('babel-register');
 require('babel-polyfill');
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 7545,
+            network_id: "*"
+        },
+        live: {
+            host: "0.0.0.0",
+            port: 8545,
+            network_id: "*",
+            gas: 3000000
+        }
+    },
+    rpc: {
+        host: "0.0.0.0",
+        port: 8545
     }
-  }
 };
